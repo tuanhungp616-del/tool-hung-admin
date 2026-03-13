4c312ad6f74c1ecc17e20e6e2e905a55121a1a5c
tuanhungp616-del <tuanhungp616@gmail.com>
2026-03-13 18:24:08 +0700

    Change Uvicorn app reference from server_ai to main

--- main.py
+++ main.py
@@ -142,5 +142,5 @@ async def home(): return FileResponse("index.html")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    uvicorn.run("server_ai:app", host="0.0.0.0", port=port)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
         
